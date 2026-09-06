be50d1992e37d209a792e0b3c109e0de665a863f
Justin <justin@layerv.ai>
2026-09-06 16:50:33 -0600

    test(client): gate proxy phase race

--- client/proxy/proxy_wrapper_test.go
+++ client/proxy/proxy_wrapper_test.go
@@ -19,6 +19,7 @@ package proxy
 import (
 	"net"
 	"sync"
+	"sync/atomic"
 	"testing"
 
 	v1 "github.com/fatedier/frp/pkg/config/v1"
@@ -26,40 +27,57 @@ import (
 	"github.com/fatedier/frp/pkg/util/xlog"
 )
 
-type closingProxy struct{}
+type closingProxy struct{ calls atomic.Int64 }
 
-func (closingProxy) Run() error { return nil }
-func (closingProxy) InWorkConn(conn net.Conn, _ *msg.StartWorkConn) {
+func (*closingProxy) Run() error { return nil }
+func (p *closingProxy) InWorkConn(conn net.Conn, _ *msg.StartWorkConn) {
+	p.calls.Add(1)
 	_ = conn.Close()
 }
 
-func (closingProxy) SetInWorkConnCallback(func(*v1.ProxyBaseConfig, net.Conn, *msg.StartWorkConn) bool) {
+func (*closingProxy) SetInWorkConnCallback(func(*v1.ProxyBaseConfig, net.Conn, *msg.StartWorkConn) bool) {
 }
-func (closingProxy) Close() {}
+func (*closingProxy) Close() {}
 
 func TestWrapperInWorkConnSynchronizesPhase(t *testing.T) {
+	proxy := &closingProxy{}
 	wrapper := &Wrapper{
 		WorkingStatus: WorkingStatus{Phase: ProxyPhaseRunning},
-		pxy:           closingProxy{},
+		pxy:           proxy,
 		xl:            xlog.New(),
 	}
-	var workers sync.WaitGroup
-	workers.Add(2)
-	go func() {
-		defer workers.Done()
-		for range 1000 {
+	done := make(chan struct{})
+	var writer sync.WaitGroup
+	writer.Go(func() {
+		phase := ProxyPhaseClosed
+		for {
+			select {
+			case <-done:
+				return
+			default:
+			}
 			wrapper.mu.Lock()
-			wrapper.Phase = ProxyPhaseClosed
+			wrapper.Phase = phase
 			wrapper.mu.Unlock()
+			if phase == ProxyPhaseClosed {
+				phase = ProxyPhaseRunning
+			} else {
+				phase = ProxyPhaseClosed
+			}
 		}
-	}()
-	go func() {
-		defer workers.Done()
-		for range 1000 {
-			conn, peer := net.Pipe()
-			wrapper.InWorkConn(conn, &msg.StartWorkConn{})
-			_ = peer.Close()
+	})
+	for range 1000 {
+		conn, peer := net.Pipe()
+		wrapper.InWorkConn(conn, &msg.StartWorkConn{})
+		var buffer [1]byte
+		if _, err := peer.Read(buffer[:]); err == nil {
+			t.Fatal("work connection remained open")
 		}
-	}()
-	workers.Wait()
+		_ = peer.Close()
+	}
+	close(done)
+	writer.Wait()
+	if proxy.calls.Load() == 0 {
+		t.Fatal("running proxy branch was not exercised")
+	}
 }
